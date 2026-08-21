250512381ed12bb9073707ff9e597eee40d5c573
squizzy-XD <coppr31@gmail.com>
2026-08-22 08:29:34 +1000

    typecasting

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-#strings
-first_name = "fakename"
-food = "pizza"
-email = "fake123@fake.com"
+#typecasting
 
-#integers
+name = "squizzy"
 age = 15
-quantity = 3
-num_of_students = 30
-
-#float
-price = 10.99
 gpa = 3.2
-distance = 5.5
-
-#boolean
 is_student = True
-for_sale = True
-is_online = True
-if is_online:
-    print("You are online!")
-else:
-    print("You are offline!")
+
+name = bool(name)
+print(name)
