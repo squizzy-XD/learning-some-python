4259d37c227266abd7da70e91a4008199e196959
squizzy-XD <coppr31@gmail.com>
2026-08-22 08:07:00 +1000

    finished variables

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ email = "fake123@fake.com"
 
 #integers
 age = 15
-print(f"you are {age}")
+quantity = 3
+num_of_students = 30
+
+#float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+#boolean
+is_student = True
+for_sale = True
+is_online = True
+if is_online:
+    print("You are online!")
+else:
+    print("You are offline!")
