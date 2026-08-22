7ae0654b5d38dea64883740e54cbf506ee71707a
squizzy-XD <coppr31@gmail.com>
2026-08-22 13:36:54 +1000

    madlibs game

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#input exercise shopping cart program
-
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
-print(f"You have bought {quantity} x {item}/s")
-print(f"Your total is: ${total}")
+#madlibs game
+adjective1 = input()
+noun1 = input()
+adjective2 = input()
+verb1 = input()
+adjective3 = input()
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
