0a9c689bf862abb5b79f0679801ca2d2845379e5
squizzy-XD <coppr31@gmail.com>
2026-08-22 13:32:21 +1000

    input excercise 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#input exercise rectangle area calc
+#input exercise shopping cart program
 
-length = float(input("enter the length: "))
-width = float(input("enter the width: "))
-area = length * width
-
-print(f"The area is: {area}cm^2")
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${total}")
