b70ce06d83da1a5e9fee494ad483ab45c1775ba6
squizzy-XD <coppr31@gmail.com>
2026-08-28 20:25:08 +1000

    collections stuff

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import time
+#collection: single variable used to store multiple values
+# list = [] ordered and changeable. Duplicates OK.
+# set = {} unordered and immutable, but add/remove OK. NO duplicates
+# tuple = () ordered and unchangeable. Duplicates OK. FASTER
 
-my_time = int(input("Enter your time in seconds: "))
-
-for x in range(my_time, 0, -1):
-    seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
-
-print("TIMES UP")
+fruits = ["apple", "orange", "banana", "coconut"]
+print(dir(fruits))
+#print(fruits[0])
+#for fruit in fruits:
+   #print(fruit)
