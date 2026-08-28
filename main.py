35562df6f660541b00bc5fb208bf439f68ba7dd4
squizzy-XD <coppr31@gmail.com>
2026-08-28 20:11:58 +1000

    countdown timer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#for loops
-#meant for a fixed number of times
+import time
 
-for x in reversed(range(1, 11)):
-    print(x)
-print("happy new year!!")
+my_time = int(input("Enter your time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIMES UP")
