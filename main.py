b466a18953e470913bcdadac644c24749d353900
squizzy-XD <coppr31@gmail.com>
2026-08-28 20:02:13 +1000

    for loops

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-#compound interest calculator project
+#for loops
+#meant for a fixed number of times
 
-principle = 0
-rate = 0
-time = 0
-
-while True:
-    principle = float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("principle cant be less than 0!")
-    else:
-        break
-
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("interest rate cant be less than 0!")
-    else:
-        break
-
-while True:
-    time = int(input("Enter the time in years: "))
-    if time < 0:
-        print("time cant be less than 0!")
-    else:
-        break
-
-total = principle * pow((1 + rate / 100), time)
-print(f"Balance after {time} year/s: ${total:.2f}")
+for x in reversed(range(1, 11)):
+    print(x)
+print("happy new year!!")
